e5fc6f85e1255b36c3a97e47071bfd68bcf830c8
ankitstha <ankitshrestha114@gmail.com>
2023-11-23 17:12:03 +0545

    added output location select

--- image_enhancer.py
+++ image_enhancer.py
@@ -1,8 +1,9 @@
+# image_enhancer.py
 import cv2
 import numpy as np
 import tkinter as tk
 from tkinter import filedialog
-
+from PIL import Image, ImageTk
 
 def enhance_image():
     # Ask the user to select an image file
@@ -31,17 +32,38 @@ def enhance_image():
     sharpened_image = cv2.filter2D(denoised_image, -1, kernel)
 
     # Display the original and enhanced images
-    cv2.imshow('Original Image', image)
-    cv2.imshow('Enhanced Image', sharpened_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    display_original_image(image)
+    display_enhanced_image(sharpened_image)
+
+    # Prompt the user to select a location to save the enhanced image
+    save_path = filedialog.asksaveasfilename(defaultextension=".jpg", filetypes=[("JPEG files", "*.jpg")])
+
+    # Check if a location was selected
+    if not save_path:
+        status_label.config(text="Enhancement canceled.")
+        return
 
     # Save the enhanced image
-    output_path = 'enhanced_image.jpg'
-    cv2.imwrite(output_path, sharpened_image)
+    cv2.imwrite(save_path, sharpened_image)
+    status_label.config(text=f"Enhanced image saved to {save_path}")
 
-    status_label.config(text=f"Enhanced image saved to {output_path}")
+def display_original_image(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = Image.fromarray(img)
+    img.thumbnail((400, 400))
+    img = ImageTk.PhotoImage(img)
 
+    original_image_label.config(image=img)
+    original_image_label.image = img
+
+def display_enhanced_image(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = Image.fromarray(img)
+    img.thumbnail((400, 400))
+    img = ImageTk.PhotoImage(img)
+
+    enhanced_image_label.config(image=img)
+    enhanced_image_label.image = img
 
 # Create the main window
 root = tk.Tk()
@@ -50,6 +72,12 @@ root.title("Image Enhancer")
 # Create and configure widgets
 select_button = tk.Button(root, text="Select Image", command=enhance_image)
 select_button.pack(pady=10)
+
+original_image_label = tk.Label(root, text="Original Image")
+original_image_label.pack()
+
+enhanced_image_label = tk.Label(root, text="Enhanced Image")
+enhanced_image_label.pack()
 
 status_label = tk.Label(root, text="")
 status_label.pack()
